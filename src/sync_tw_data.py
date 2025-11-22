160d983e435bac672019cd5f025a801d31324c50
Adin Chen <adinchen@Mac.localdomain>
2025-11-23 02:19:38 +0800

    feat: add lambda function to sync buy and sell data in Taiwan Market to MongoDB

---
+++ src/sync_tw_data.py
@@ -0,0 +1,24 @@
+import logging
+from quote.tw_stock import sync_all_buy_sell_today_result_to_db
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def handler(event, context):
+    """
+    Lambda handler to trigger the synchronization of daily buy/sell data.
+    This function is intended to be called by a scheduled event.
+    """
+    logger.info("Starting daily trade data synchronization job.")
+    try:
+        sync_all_buy_sell_today_result_to_db()
+        logger.info("Successfully completed daily trade data synchronization job.")
+        return {
+            'statusCode': 200,
+            'body': 'Synchronization successful.'
+        }
+    except Exception as e:
+        logger.error(f"Failed to execute daily trade data synchronization job: {e}")
+        logger.exception(e)
+        raise e
